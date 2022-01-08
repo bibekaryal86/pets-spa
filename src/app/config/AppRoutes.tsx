13a30324ca6c9ca56dc736aefbef39e8cddccac6
Bibek Aryal <bibekaryal86@gmail.com>
2022-01-07 21:36:10 -0700

    Improve AppRoutes

--- src/app/config/AppRoutes.tsx
+++ src/app/config/AppRoutes.tsx
@@ -26,124 +26,66 @@ const AppRoutes = (): React.ReactElement => {
       <Route path={'/'} element={<SignInContainer />} />
       <Route path={'/signout'} element={<SignOutContainer />} />
 
-      <Route
-        path={'/summary'}
-        element={
-          <RequireAuth>
-            <Summary />
-          </RequireAuth>
-        }
-      />
+      <Route path={'/summary'} element={getRequireAuth(<Summary />)} />
       <Route
         path={'/transactions'}
-        element={
-          <RequireAuth>
-            <TransactionsContainer />
-          </RequireAuth>
-        }
+        element={getRequireAuth(<TransactionsContainer />)}
       />
       <Route
         path={'/transaction'}
-        element={
-          <RequireAuth>
-            <OneTransactionContainer />
-          </RequireAuth>
-        }
+        element={getRequireAuth(<OneTransactionContainer />)}
       >
         <Route
           path={':id'}
-          element={
-            <RequireAuth>
-              <OneTransactionContainer />
-            </RequireAuth>
-          }
+          element={getRequireAuth(<OneTransactionContainer />)}
         />
       </Route>
       <Route
         path={'/accounts'}
-        element={
-          <RequireAuth>
-            <AccountsContainer />
-          </RequireAuth>
-        }
+        element={getRequireAuth(<AccountsContainer />)}
       />
       <Route
         path={'/account'}
-        element={
-          <RequireAuth>
-            <OneAccountContainer />
-          </RequireAuth>
-        }
+        element={getRequireAuth(<OneAccountContainer />)}
       >
-        <Route
-          path={':id'}
-          element={
-            <RequireAuth>
-              <OneAccountContainer />
-            </RequireAuth>
-          }
-        />
+        <Route path={':id'} element={getRequireAuth(<OneAccountContainer />)} />
       </Route>
       <Route
         path={'/merchants'}
-        element={
-          <RequireAuth>
-            <MerchantsContainer />
-          </RequireAuth>
-        }
+        element={getRequireAuth(<MerchantsContainer />)}
       />
       <Route
         path={'/merchant'}
-        element={
-          <RequireAuth>
-            <OneMerchantContainer />
-          </RequireAuth>
-        }
+        element={getRequireAuth(<OneMerchantContainer />)}
       >
         <Route
           path={':id'}
-          element={
-            <RequireAuth>
-              <OneMerchantContainer />
-            </RequireAuth>
-          }
+          element={getRequireAuth(<OneMerchantContainer />)}
         />
       </Route>
       <Route
         path={REPORT_PATH_ALL}
-        element={
-          <RequireAuth>
-            <ReportsContainer />
-          </RequireAuth>
-        }
+        element={getRequireAuth(<ReportsContainer />)}
       />
       <Route
         path={REPORT_PATH_CURRENT_BALANCES}
-        element={
-          <RequireAuth>
-            <ReportsContainer />
-          </RequireAuth>
-        }
+        element={getRequireAuth(<ReportsContainer />)}
       />
       <Route
         path={REPORT_PATH_CASH_FLOWS}
-        element={
-          <RequireAuth>
-            <ReportsContainer />
-          </RequireAuth>
-        }
+        element={getRequireAuth(<ReportsContainer />)}
       />
       <Route
         path={REPORT_PATH_CATEGORIES}
-        element={
-          <RequireAuth>
-            <ReportsContainer />
-          </RequireAuth>
-        }
+        element={getRequireAuth(<ReportsContainer />)}
       />
     </Routes>
   );
 };
+
+const getRequireAuth = (children: React.ReactElement) => (
+  <RequireAuth>{children}</RequireAuth>
+);
 
 function RequireAuth({ children }: { children: React.ReactElement }) {
   const location = useLocation();
